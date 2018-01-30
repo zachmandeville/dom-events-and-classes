18388dc84c23e560ca9264e36cae867babd1f092
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-01-30 00:17:36 -0800

    added blue on click

--- game.js
+++ game.js
@@ -12,8 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
-  }
-}
+    dots[i].addEventListener('click', makeBlue) }
+    }
 
 function makeGreen (evt) {
   evt.preventDefault()
@@ -22,6 +22,9 @@ function makeGreen (evt) {
 }
 
 // CREATE FUNCTION makeBlue HERE
+function makeBlue(evt) {
+  evt.target.classList.toggle('blue');
+  updateCounts(); }
 
 // CREATE FUNCTION hide HERE
 
